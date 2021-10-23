b0c5fef9946b6b7a09eae3c7df4094e933b18ef3
Anton <antonbogatirev1@gmail.com>
2021-10-22 21:56:42 -0700

    Adding signin functionality
    Added a signin function and route.

--- warbler-server/handlers/auth.js
+++ warbler-server/handlers/auth.js
@@ -1,17 +1,48 @@
 const db = require("../models");
 const jwt = require("jsonwebtoken");
 
-exports.signin = function() {};
+exports.signin = async function (req, res, next) {
+    try {
+        let user = await db.User.findOne({
+            email: req.body.email
+        });
+        let { id, username, profileImageUrl } = user;
+        let isMatch = await user.comparePassword(req.body.password);
+        if (isMatch) {
+            let token = jwt.sign(
+                {
+                    id,
+                    username,
+                    profileImageUrl
+                },
+                process.env.SECRET_KEY
+            );
+            return res.status(200).json({
+                id,
+                username,
+                profileImageUrl,
+                token
+            });
+        } else {
+            return next({
+                status: 400,
+                message: "Invalid Email/Password."
+            });
+        };
+    } catch (e) {
+        return next({ status: 400, message: "Invalid Email/Password." });
+    };
+};;
 
-exports.signup = async function(req, res, next) {
+exports.signup = async function (req, res, next) {
     try {
         let user = await db.User.create(req.body);
         let { id, username, profileImageUrl } = user;
         let token = jwt.sign(
             {
-            id,
-            username,
-            profileImageUrl
+                id,
+                username,
+                profileImageUrl
             },
             process.env.SECRET_KEY
         );
