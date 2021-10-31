a09bc8b4219bb37d3393f75e4aa085162f8ec67d
Anton <antonbogatirev1@gmail.com>
2021-10-31 16:41:13 -0700

    Adding Login/Sign Up form
    Added a AuthForm component.
    Displays username and password inputs to log in.
    Also acts as a sign up form which would also display email and image url inputs.

---
+++ warbler-client/src/components/AuthForm.js
@@ -0,0 +1,77 @@
+import React, { Component } from "react";
+import { render } from "react-dom";
+
+export default class AuthForm extends Component {
+    constructor(props) {
+        super(props); // super gets the parent props
+        this.state = {
+            email: "",
+            username: "",
+            password: "",
+            profileImageUrl: ""
+        };
+    };
+
+    // generic handleChange
+    handleChange = e => { // using arrow functions to get the correct value of the keyword 'this'
+        this.setState({
+            [e.target.name]: e.target.value
+        });
+    };
+
+    render() {
+        const { email, username, password, profileImageUrl } = this.state;
+        const { heading, buttonText, signUp } = this.props;
+        return (
+            <div>
+                <div className="row justify-content-md-center text-center">
+                    <div className="col-md-6">
+                        <form onSubmit={this.handleSubmit}>
+                            <h2>{heading}</h2>
+                            <label htmlFor="email">Email:</label>
+                            <input
+                                className="form-control"
+                                id="email"
+                                name="email"
+                                onChange={this.handleChange}
+                                value={email}
+                                type="text"
+                            />
+                            <label htmlFor="password">Password:</label>
+                            <input
+                                className="form-control"
+                                id="password"
+                                name="password"
+                                onChange={this.handleChange}
+                                type="password"
+                            />
+                            {signUp && ( // if the prop "signUp" is passed, show the following two inputs
+                                <div>
+                                    <label htmlFor="username">Username:</label>
+                                    <input
+                                        className="form-control"
+                                        id="username"
+                                        name="username"
+                                        onChange={this.handleChange}
+                                        value={username}
+                                        type="text"
+                                    />
+                                    <label htmlFor="image-url">Image URL:</label>
+                                    <input
+                                        className="form-control"
+                                        id="image-url"
+                                        name="profileImageUrl"
+                                        onChange={this.handleChange}
+                                        type="text"
+                                        value={profileImageUrl}
+                                    />
+                                </div>
+                            )}
+                        </form>
+                    </div>
+                </div>
+            </div>
+        )
+    };
+};
+
--- warbler-client/src/containers/Main.js
+++ warbler-client/src/containers/Main.js
@@ -2,12 +2,23 @@ import React from "react"; // Stateless functional component so we dont need to 
 import { Switch, Route, withRouter, Redirect } from "react-router-dom"; // Helps us pass props down
 import { connect } from "react-redux";
 import Homepage from "../components/Homepage";
+import AuthForm from "../components/AuthForm";
 
 const Main = props => {
     return (
         <div className="container">
             <Switch>
                 <Route exact path="/" render={props => <Homepage {...props} />} />
+                <Route exact path="/signin" render={props => {
+                    return (
+                        <AuthForm buttonText="Log in" heading="Welcome Back"{...props} />
+                    )
+                }} />
+                <Route exact path="/signup" render={props => {
+                    return (
+                        <AuthForm signUp buttonText="Sign me up!" heading="Join Warbler today!"{...props} />
+                    )
+                }} />
             </Switch>
         </div>
     );
