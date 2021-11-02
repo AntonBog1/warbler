2427f5bf67350d299d458dd06d0627907f0802cc
Anton <antonbogatirev1@gmail.com>
2021-11-02 13:23:28 -0700

    Showing and removing error messages
    Added addError & removeError actions.
    Errors will show up as an alert on the login/singup page and will disappear when a user changes pages.

--- warbler-client/src/containers/Main.js
+++ warbler-client/src/containers/Main.js
@@ -4,21 +4,37 @@ import { connect } from "react-redux";
 import Homepage from "../components/Homepage";
 import AuthForm from "../components/AuthForm";
 import { authUser } from "../store/actions/auth";
+import { removeError } from "../store/actions/errors";
 
 const Main = props => {
-    const { authUser } = props;
+    const { authUser, errors, removeError } = props;
     return (
         <div className="container">
             <Switch>
                 <Route exact path="/" render={props => <Homepage {...props} />} />
                 <Route exact path="/signin" render={props => {
                     return (
-                        <AuthForm onAuth={authUser} buttonText="Log in" heading="Welcome Back"{...props} />
+                        <AuthForm
+                            removeError={removeError}
+                            errors={errors}
+                            onAuth={authUser}
+                            buttonText="Log in"
+                            heading="Welcome Back" 
+                            {...props}
+                        />
                     )
                 }} />
                 <Route exact path="/signup" render={props => {
                     return (
-                        <AuthForm onAuth={authUser} signUp buttonText="Sign me up!" heading="Join Warbler today!"{...props} />
+                        <AuthForm
+                            removeError={removeError}
+                            errors={errors}
+                            onAuth={authUser}
+                            signUp
+                            buttonText="Sign me up!"
+                            heading="Join Warbler today!"
+                            {...props}
+                        />
                     )
                 }} />
             </Switch>
@@ -28,8 +44,11 @@ const Main = props => {
 
 function mapStateToProps(state) {
     return { //Keys here will be placed onto props
-        currentUser: state.currentUser
+        currentUser: state.currentUser,
+        errors: state.errors
     };
 };
 
-export default withRouter(connect(mapStateToProps, { authUser })(Main)); //Allows us to get the props from the router to the Homepage component. Also allows the use of the history object. 
+export default withRouter(
+    connect(mapStateToProps, { authUser, removeError })(Main)
+    ); //Allows us to get the props from the router to the Homepage component. Also allows the use of the history object. 
--- warbler-client/src/services/api.js
+++ warbler-client/src/services/api.js
@@ -13,7 +13,7 @@ export function apiCall(method, path, data) {
                 return resolve(res.data);
             })
             .catch(err => {
-                return reject(err.response.data.err);
+                return reject(err.response.data.error);
             });
     });
 };
